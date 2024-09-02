68564fd18460f20b15507163fc0fe16199f5905a
fernado gonzalez <tupapirico204@hotmail.com>
2024-09-01 22:33:09 -0500

    Agregar archivos de proyecto.

---
+++ MVCAJAX/Models/ExamenContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCAJAX_FERNANDO.Models;
+
+namespace MVCAJAX_FERNANDO.Models
+{
+    public class ExamenContext : DbContext
+    {
+        public DbSet<Productos> Productos { get; set; }
+
+        public ExamenContext(DbContextOptions<ExamenContext> options) : base(options) 
+        { 
+        }
+    }
+}
---
+++ MVCAJAX/Models/Productos.cs
@@ -0,0 +1,10 @@
+﻿namespace MVCAJAX_FERNANDO.Models
+{
+    public class Productos
+    {
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public DateTime FechaVencimiento { get; set; }
+    }
+}
